4dd94cf38d79b09e76042a32f792b1dd590f9fcb
AlejandroPortilla <alejandroe.portilla221@umariana.edu.co>
2023-08-27 16:22:22 -0500

    Update Proyecto1.java

--- src/main/java/com/mycompany/proyecto1/Proyecto1.java
+++ src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -4,10 +4,10 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.ArrayList;
 import mundo.Alumno;
-import java.io.FileWriter;
 import java.io.IOException;
 
 
